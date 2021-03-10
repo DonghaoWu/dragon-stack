3d4c3f17f920e550c18dede5f21700e5783dd762
Donghao Wu <donghao.wu.us@gmail.com>
2021-03-10 13:56:18 -0500

    changed file organization.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import store from './store';
+import store from './redux/store';
 import { fetchAuthenticated } from './actions/accountActions';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import AccountDragons from './components/AccountDragons';
