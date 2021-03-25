465bb986b4163107859a9a679947b3feb663a07b
Donghao Wu <donghao.wu.us@gmail.com>
2021-03-24 23:32:46 -0400

    clean up code.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import store from './redux/store';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
-
 import App from './App';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
+import store from './redux/store';
 
 import { fetchAuthenticated } from './redux/actions/accountActions';
 import { fetchPublicDragons } from './redux/actions/publicDragonActions';
