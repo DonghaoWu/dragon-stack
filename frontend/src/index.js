9f27f5d4d10c918fef6946899444ef4dd3e47055
Donghao Wu <donghao.wu.us@gmail.com>
2021-04-07 23:20:21 -0400

    edited index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
 
+import store from './redux/store';
+import { fetchAuthenticated } from './redux/actions/accountActions';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-
-import store from './redux/store';
-
-import { fetchAuthenticated } from './redux/actions/accountActions';
 
 store.dispatch(fetchAuthenticated)
   .then(() => {
